c80d7b7dc8d2a6f7d9a2ba034a80074b3c8066de
dickyukong121 <62990439+dickyukong121@users.noreply.github.com>
2022-05-16 23:14:56 +0800

    Finish without unit test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    transform: {
+        "^.+\\.jsx?$": 'babel-jest',
+
+        "^.+\\.scss$": 'jest-scss-transform',
+    },
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,20 @@
+import Input from './components/input/input.component';
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import { createStore } from 'redux';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { Provider } from 'react-redux';
+import { dataReducer } from "./store/data/data.reducer";
 
-test('Check the result of 5 + ', () => {
-  expect(5 + 2).toBe(7)
-})
 
+describe('Test <Input/>', () => {
+  afterEach(cleanup);
+  test('Test reducer render Input', () => {
+
+    const { getByTestId, getByText, container, } = render(<Input />);
+
+    expect(getByText('1').textContent).toBe('1');
+
+  });
+});
